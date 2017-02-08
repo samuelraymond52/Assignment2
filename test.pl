1e6cb30d220b841ce6329449ae501790675377bd
samuelraymond52 <samuel.raymond@bison.howard.edu>
2017-02-08 14:19:11 -0500

    labeled questions and submitted the second case

--- test.pl
+++ test.pl
@@ -1,3 +1,4 @@
+/* Question 1*/
 sum-up-numbers-simple([],0). /* If list is empty set to 0*/
 sum-up-numbers-simple([A|Z],N) :- 
 number(A), 
@@ -6,7 +7,7 @@ N is A + Summ. /* If the current element is a number recursively add the element
 sum-up-numbers-simple([A|Z],N) :-
 \+number(A),sum-up-numbers-simple(Z,N). /*If the current element is not a number recursivly continue through the list */
 
-
+/* Question 2*/
 sum-up-numbers-general([],0). /* If the list is empty return 0*/
 sum-up-numbers-general([[]],0). /*if the lst inside the list is empty return 0*/
 sum-up-numbers-general([A|Z],N) :-
@@ -20,7 +21,12 @@ sum-up-numbers-general([A|Z],N) :-
 sum-up-numbers-general(Z,Summ),
 N is A + Summ. /* check in the atom if it is a list and add the value recurslivly if it is a number*/
 
-common-unique-elements([],_,[])
 
+/* Question 4 */
+common-unique-elements([],_,[]) /* Checks if the current atom in the list is a list, then returns a list*/
+common-unique-elements([N|E1], E2, [N|E3]):-
+	member(E2,N),
+	!,
+	common-unique-elements(E1,E2,E3).
 
 
