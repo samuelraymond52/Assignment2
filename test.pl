f3d357fe8207101c1b1fb56fe6adddbac1c9d9a6
samuelraymond52 <samuel.raymond@bison.howard.edu>
2017-02-08 04:11:16 -0500

    added comments for the second test case

--- test.pl
+++ test.pl
@@ -4,4 +4,4 @@ number(A),
 sum-up-numbers-simple(Z,Summ), 
 N is A + Summ. /* If the current element is a number recursively add the elements in the list*/
 sum-up-numbers-simple([A|Z],N) :-
-\+number(A),sum-up-numbers-simple(Z,N). /* */
+\+number(A),sum-up-numbers-simple(Z,N). /*If the current element is not a number recursivly continue through the list */
