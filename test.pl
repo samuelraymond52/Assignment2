9e1e0caa05baace2c40a24bee98351ba30a7d02a
samuelraymond52 <samuel.raymond@bison.howard.edu>
2017-02-08 04:09:49 -0500

    second querey that checks if the atoms are numbers, if they are not continue throuh list

---
+++ test.pl
@@ -0,0 +1,7 @@
+sum-up-numbers-simple([],0). /* If list is empty set to 0*/
+sum-up-numbers-simple([A|Z],N) :- 
+number(A), 
+sum-up-numbers-simple(Z,Summ), 
+N is A + Summ. /* If the current element is a number recursively add the elements in the list*/
+sum-up-numbers-simple([A|Z],N) :-
+\+number(A),sum-up-numbers-simple(Z,N). /* */
