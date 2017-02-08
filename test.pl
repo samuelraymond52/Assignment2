ba4d2899e18dad76a1defecd186bfc7fbe891fac
samuelraymond52 <samuel.raymond@bison.howard.edu>
2017-02-08 05:26:10 -0500

    Third case in the function
    If the atom is a number add it recursivley

--- test.pl
+++ test.pl
@@ -9,3 +9,8 @@ sum-up-numbers-simple([A|Z],N) :-
 
 sum-up-numbers-general([],0). /* If the list is empty return 0*/
 sum-up-numbers-general([[]],0). /*if the lst inside the list is empty return 0*/
+sum-up-numbers-general([A|Z],N) :-
+number(A), 
+sum-up-numbers-general(Z,Summ),
+N is A + Summ. /*if the atom is a number recursivly add the number in the list*/
+
